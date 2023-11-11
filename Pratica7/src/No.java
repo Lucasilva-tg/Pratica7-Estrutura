437ad3ca386ea9c51a3f8be538ddc103d4c5102b
Lucasilva-tg <101115192+Lucasilva-tg@users.noreply.github.com>
2023-11-10 21:38:57 -0300

    Add files via upload

---
+++ Pratica7/src/No.java
@@ -0,0 +1,13 @@
+public class No {
+    int valor;
+    int altura; // Nova propriedade para armazenar a altura do nó
+    No esquerda;
+    No direita;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.altura = 1; // Inicializa a altura como 1 para um nó recém-criado
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
